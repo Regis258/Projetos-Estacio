d2b6d3b3bd0f3dc39cf182da10b4a19873cd4ce1
Regis258 <reginaldopalafoz@gmail.com>
2025-07-14 14:53:21 -0300

    alterações

--- OlaMundo/OlaMundo.c
+++ OlaMundo/OlaMundo.c
@@ -6,5 +6,12 @@ int main()
     // Define a localização para português do Brasil
     setlocale(LC_ALL, "Portuguese");
 
-    printf("Olá Mundo! \n");
+    int idade = 28;      // variáveis de numeros inteiros por exemplo: 10, 20, 30, 40, 50 etc.
+    int quantidade = 1;  // variáveis de numeros inteiros por exemplo: 10, 20, 30, 40, 50 etc.
+    float altura = 1.63; // variáveis de números Reais (Com casas decimais) exemplo: 1,70. 1,85. 3,895487. etc.
+    double peso = 66.5;  // Variavel de numeros reais que serve para trabalhar com mais casas decimais que a variavel float.
+    char letra = 'A';
+    char nome[10] = "Regis"; // Variavel que é usada pra Arrays com o numero de quantidade de caracteres qeu será utilizada.
+
+    printf("Ola Mundo! \n");
 }
